f41578dcd45c7084ff5e7ef8d002110100363cba
Vlhermitte <valentin.lhermitte2604@gmail.com>
2024-11-05 13:27:31 +0100

    add repo cloning script

---
+++ clone_repos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clone the SFM approaches (Glomap, VGGSfm, Flowmap, AceZero)
+
+# Clone Glomap
+git clone https://github.com/Vlhermitte/glomap.git
+
+# Clone VGGSfm
+git clone https://github.com/Vlhermitte/vggsfm.git
+
+# Clone Flowmap
+git clone https://github.com/Vlhermitte/flowmap.git
+
+# Clone AceZero
+git clone https://github.com/Vlhermitte/acezero.git
+
+# Clone Tank and Temples (Dataset)
+git clone https://github.com/Vlhermitte/TanksAndTemples.git
