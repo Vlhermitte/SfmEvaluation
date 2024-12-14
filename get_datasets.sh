ce0514eebce515e650f2be7613fb55545fe18f7e
Vlhermitte <valentin.lhermitte2604@gmail.com>
2024-12-14 11:42:11 +0100

    Adding a download script for the datasets

---
+++ get_datasets.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ETH3D Dataset (all 12 scenes)
+wget https://www.eth3d.net/data/courtyard_dslr_jpg.7z
+wget https://www.eth3d.net/data/delivery_area_dslr_jpg.7z
+wget https://www.eth3d.net/data/electro_dslr_jpg.7z
+wget https://www.eth3d.net/data/facade_dslr_jpg.7z
+wget https://www.eth3d.net/data/kicker_dslr_jpg.7z
+wget https://www.eth3d.net/data/meadow_dslr_jpg.7z
+wget https://www.eth3d.net/data/office_dslr_jpg.7z
+wget https://www.eth3d.net/data/pipes_dslr_jpg.7z
+wget https://www.eth3d.net/data/playground_dslr_jpg.7z
+wget https://www.eth3d.net/data/relief_dslr_jpg.7z
+wget https://www.eth3d.net/data/relief_2_dslr_jpg.7z
+wget https://www.eth3d.net/data/terrace_dslr_jpg.7z
+wget https://www.eth3d.net/data/terrains_dslr_jpg.7z
+
+
+# TODO: Add the other datasets here (Tanks and Temples, MipNef360, etc.)
