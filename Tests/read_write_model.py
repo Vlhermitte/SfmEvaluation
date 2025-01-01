bb7a461d403ed262b9e2aaccdcd584f16c68cae4
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-01-01 18:03:04 +0100

    Adding model file existence to avoid crashing

--- Tests/read_write_model.py
+++ Tests/read_write_model.py
@@ -497,15 +497,22 @@ def read_model(path, ext=""):
         else:
             print("Provide model format: '.bin' or '.txt'")
             return
-
+    cameras, images, points3D = {}, {}, {}
     if ext == ".txt":
-        cameras = read_cameras_text(os.path.join(path, "cameras" + ext))
-        images = read_images_text(os.path.join(path, "images" + ext))
-        points3D = read_points3D_text(os.path.join(path, "points3D") + ext)
+        # if file exists, read the model
+        if os.path.isfile(os.path.join(path, "cameras" + ext)):
+            cameras = read_cameras_text(os.path.join(path, "cameras" + ext))
+        if os.path.isfile(os.path.join(path, "images" + ext)):
+            images = read_images_text(os.path.join(path, "images" + ext))
+        if os.path.isfile(os.path.join(path, "points3D" + ext)):
+            points3D = read_points3D_text(os.path.join(path, "points3D") + ext)
     else:
-        cameras = read_cameras_binary(os.path.join(path, "cameras" + ext))
-        images = read_images_binary(os.path.join(path, "images" + ext))
-        points3D = read_points3D_binary(os.path.join(path, "points3D") + ext)
+        if os.path.isfile(os.path.join(path, "cameras" + ext)):
+            cameras = read_cameras_binary(os.path.join(path, "cameras" + ext))
+        if os.path.isfile(os.path.join(path, "images" + ext)):
+            images = read_images_binary(os.path.join(path, "images" + ext))
+        if os.path.isfile(os.path.join(path, "points3D" + ext)):
+            points3D = read_points3D_binary(os.path.join(path, "points3D") + ext)
     return cameras, images, points3D
 
 
