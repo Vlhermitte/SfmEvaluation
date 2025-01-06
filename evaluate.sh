84bb90a76475bbfadbf5055dfc1a3bde81cbb713
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-01-07 00:24:28 +0100

    Add acezero and glomap to evaluation.sh

--- evaluate.sh
+++ evaluate.sh
@@ -24,6 +24,7 @@ for scene in "${SCENES[@]}"; do
     echo "VGGSfm model not found for ${scene}. Skipping..."
     continue
   fi
+  echo "VGGSfm: Processing ${scene}"
   python Tests/main.py --est-model-path results/vggsfm/${scene}/sparse --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
 done
 
@@ -34,5 +35,28 @@ for scene in "${SCENES[@]}"; do
     echo "Flowmap model not found for ${scene}. Skipping..."
     continue
   fi
+  echo "Flowmap: Processing ${scene}"
   python Tests/main.py --est-model-path results/flowmap/${scene}/colmap/sparse/0 --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
+done
+
+# AceZero
+for scene in "${SCENES[@]}"; do
+  # Check if the estimated model exists
+  if [ ! -d "results/flowmap/${scene}/colmap/sparse/0" ]; then
+    echo "Flowmap model not found for ${scene}. Skipping..."
+    continue
+  fi
+  echo "AceZero: Processing ${scene}"
+  python Tests/main.py --est-model-path results/acezero/${scene}/sparse/ --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
+done
+
+# Glomap
+for scene in "${SCENES[@]}"; do
+  # Check if the estimated model exists
+  if [ ! -d "results/glomap/${scene}/sparse/0" ]; then
+    echo "Glomap model not found for ${scene}. Skipping..."
+    continue
+  fi
+  echo "Glomap: Processing ${scene}"
+  python Tests/main.py --est-model-path results/glomap/${scene}/sparse/0 --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
 done
