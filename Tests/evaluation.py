4566272f193a2f64f8394713506c2bf206a9c541
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-01-09 15:48:51 +0100

    restructure code

--- Tests/evaluation.py
+++ Tests/evaluation.py
@@ -1,6 +1,9 @@
 import numpy as np
-from common import Camera
+from common import Camera, get_cameras_info
 from typing import List, Dict
+from read_write_model import read_model
+from alignment import estimate_alignment
+from interpolation import interpolate_missing_cameras
 
 
 def evaluate_rotation_matrices(R_gt: np.ndarray, R_est: np.ndarray) -> float:
@@ -146,3 +149,34 @@ def report_metrics(results, verbose: bool = False) -> tuple[dict, dict]:
             print(f"  Range: [{metric_stats['min']:.3f}, {metric_stats['max']:.3f}]")
 
     return stats, distributions
+
+def run_evaluation(est_model_path: str, gt_model_path: str, verbose: bool = False) -> Tuple[dict, dict]:
+    # Estimated model
+    est_cameras_type, images, est_points3D = read_model(est_model_path)
+    # Ground truth model
+    gt_cameras_type, gt_images, gt_points3D = read_model(gt_model_path, '.txt')
+
+    # Create Open3D point cloud and get R and t for estimated and ground truth models
+    est_cameras = get_cameras_info(est_cameras_type, images)
+    gt_cameras = get_cameras_info(gt_cameras_type, gt_images)
+
+    if len(est_cameras) != len(gt_cameras):
+        # Interpolate missing cameras in the estimated set using neighboring cameras
+        if verbose:
+            print('Missing cameras in the estimated model. Interpolating...')
+        est_cameras = interpolate_missing_cameras(est_cameras, gt_cameras)
+
+    # Sort the cameras in estimated cameras based on the image name to match the ground truth
+    gt_camera_order = {camera.image: idx for idx, camera in enumerate(gt_cameras)}
+    est_cameras = sorted(
+        est_cameras,
+        key=lambda camera: gt_camera_order.get(camera.image, float('inf'))
+    )
+
+    # Evaluating
+    results = evaluate_relative_errors(est_cameras=est_cameras, gt_cameras=gt_cameras)
+    stats, distributions = report_metrics(results, verbose=verbose)
+
+    # Plot the error distributions
+    plot_error_distributions(results, save_path=est_model_path)
+    plot_cumulative_errors(results, save_path=est_model_path)
--- Tests/main.py
+++ Tests/main.py
@@ -1,45 +1,7 @@
-from typing import Tuple
-import copy
-import numpy as np
 import argparse
-import matplotlib.pyplot as plt
+from evaluation import run_evaluation
+from absolute_error_evaluation import evaluate_camera_pose
 
-from read_write_model import read_model
-from common import Camera, get_cameras_info
-from evaluation import evaluate_relative_errors, report_metrics
-from plotting import plot_error_distributions, plot_cumulative_errors
-from interpolation import interpolate_missing_cameras
-
-def run_evaluation(est_model_path: str, gt_model_path: str, verbose: bool = False) -> Tuple[dict, dict]:
-    # Estimated model
-    est_cameras_type, images, est_points3D = read_model(est_model_path)
-    # Ground truth model
-    gt_cameras_type, gt_images, gt_points3D = read_model(gt_model_path, '.txt')
-
-    # Create Open3D point cloud and get R and t for estimated and ground truth models
-    est_cameras = get_cameras_info(est_cameras_type, images)
-    gt_cameras = get_cameras_info(gt_cameras_type, gt_images)
-
-    if len(est_cameras) != len(gt_cameras):
-        # Interpolate missing cameras in the estimated set using neighboring cameras
-        if verbose:
-            print('Missing cameras in the estimated model. Interpolating...')
-        est_cameras = interpolate_missing_cameras(est_cameras, gt_cameras)
-
-    # Sort the cameras in estimated cameras based on the image name to match the ground truth
-    gt_camera_order = {camera.image: idx for idx, camera in enumerate(gt_cameras)}
-    est_cameras = sorted(
-        est_cameras,
-        key=lambda camera: gt_camera_order.get(camera.image, float('inf'))
-    )
-
-    # Evaluating
-    results = evaluate_relative_errors(est_cameras=est_cameras, gt_cameras=gt_cameras)
-    stats, distributions = report_metrics(results, verbose=verbose)
-
-    # Plot the error distributions
-    plot_error_distributions(results, save_path=est_model_path)
-    plot_cumulative_errors(results, save_path=est_model_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
