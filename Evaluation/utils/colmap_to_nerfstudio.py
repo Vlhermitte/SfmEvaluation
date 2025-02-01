4b26f0b8accd1eaba6e0ddae2e00af4d784b254a
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-02-01 14:11:29 +0100

    Remove unnecessary code for this project

--- Evaluation/utils/colmap_to_nerfstudio.py
+++ Evaluation/utils/colmap_to_nerfstudio.py
@@ -1,4 +1,4 @@
-# This project uses code from NerfStudio, which is licensed under the Apache License 2.0.
+# This file uses a modified code from NerfStudio, which is licensed under the Apache License 2.0.
 # Copyright (c) 2022 The Regents of the University of California, Nerfstudio Team.
 # The original code can be found at https://github.com/nerfstudio-project/nerfstudio/blob/main/nerfstudio/process_data/colmap_utils.py
 
@@ -11,22 +11,10 @@ import numpy as np
 from read_write_model import read_cameras_binary, read_images_binary
 from Evaluation.core.geometry import quaternion2rotation
 
-class CameraModel(Enum):
-    """Enum for camera types."""
-
-    OPENCV = "OPENCV"
-    OPENCV_FISHEYE = "OPENCV_FISHEYE"
-    EQUIRECTANGULAR = "EQUIRECTANGULAR"
-    PINHOLE = "PINHOLE"
-    SIMPLE_PINHOLE = "SIMPLE_PINHOLE"
-
 def colmap_to_json(
     recon_dir: Path,
     output_dir: Path,
-    camera_mask_path: Optional[Path] = None,
-    image_id_to_depth_path: Optional[Dict[int, Path]] = None,
-    image_rename_map: Optional[Dict[str, str]] = None,
-    ply_filename="sparse_pc.ply",
+    image_dir: Optional[Path] = None,
     keep_original_world_coordinate: bool = False,
     use_single_camera_mode: bool = True,
 ) -> int:
@@ -35,21 +23,13 @@ def colmap_to_json(
     Args:
         recon_dir: Path to the reconstruction directory, e.g. "sparse/0"
         output_dir: Path to the output directory.
-        camera_model: Camera model used.
-        camera_mask_path: Path to the camera mask.
-        image_id_to_depth_path: When including sfm-based depth, embed these depth file paths in the exported json
-        image_rename_map: Use these image names instead of the names embedded in the COLMAP db
+        image_dir: Path to the image directory.
         keep_original_world_coordinate: If True, no extra transform will be applied to world coordinate.
                     Colmap optimized world often have y direction of the first camera pointing towards down direction,
                     while nerfstudio world set z direction to be up direction for viewer.
     Returns:
         The number of registered images.
     """
-
-    # TODO(1480) use pycolmap
-    # recon = pycolmap.Reconstruction(recon_dir)
-    # cam_id_to_camera = recon.cameras
-    # im_id_to_image = recon.images
     cam_id_to_camera = read_cameras_binary(recon_dir / "cameras.bin")
     im_id_to_image = read_images_binary(recon_dir / "images.bin")
     if set(cam_id_to_camera.keys()) != {1}:
@@ -62,41 +42,29 @@ def colmap_to_json(
 
     frames = []
     for im_id, im_data in im_id_to_image.items():
-        # NB: COLMAP uses Eigen / scalar-first quaternions
-        # * https://colmap.github.io/format.html
-        # * https://github.com/colmap/colmap/blob/bf3e19140f491c3042bfd85b7192ef7d249808ec/src/base/pose.cc#L75
-        # the `rotation_matrix()` handles that format for us.
-
-        # TODO(1480) BEGIN use pycolmap API
-        # rotation = im_data.rotation_matrix()
         rotation = quaternion2rotation(im_data.qvec)
 
         translation = im_data.tvec.reshape(3, 1)
         w2c = np.concatenate([rotation, translation], 1)
         w2c = np.concatenate([w2c, np.array([[0, 0, 0, 1]])], 0)
         c2w = np.linalg.inv(w2c)
-        # Convert from COLMAP's camera coordinate system (OpenCV) to ours (OpenGL)
+        # Convert from COLMAP's camera coordinate system (OpenCV) to nerfstudio (OpenGL)
         c2w[0:3, 1:3] *= -1
         if not keep_original_world_coordinate:
             c2w = c2w[np.array([0, 2, 1, 3]), :]
             c2w[2, :] *= -1
 
         name = im_data.name
-        if image_rename_map is not None:
-            name = image_rename_map[name]
-        name = Path(f"./images/{name}")
+        if image_dir:
+            name = Path(f"{image_dir}/{name}")
+        else:
+            name = Path(f"./images/{name}")
 
         frame = {
             "file_path": name.as_posix(),
             "transform_matrix": c2w.tolist(),
             "colmap_im_id": im_id,
         }
-        if camera_mask_path is not None:
-            frame["mask_path"] = camera_mask_path.relative_to(camera_mask_path.parent.parent).as_posix()
-        if image_id_to_depth_path is not None:
-            depth_path = image_id_to_depth_path[im_id]
-            frame["depth_file_path"] = str(depth_path.relative_to(depth_path.parent.parent))
-
         if not use_single_camera_mode:  # add the camera parameters for this frame
             frame.update(parse_colmap_camera_params(cam_id_to_camera[im_data.camera_id]))
 
@@ -110,16 +78,6 @@ def colmap_to_json(
         applied_transform = applied_transform[np.array([0, 2, 1]), :]
         applied_transform[2, :] *= -1
         out["applied_transform"] = applied_transform.tolist()
-
-    # create ply from colmap
-    # assert ply_filename.endswith(".ply"), f"ply_filename: {ply_filename} does not end with '.ply'"
-    # create_ply_from_colmap(
-    #     ply_filename,
-    #     recon_dir,
-    #     output_dir,
-    #     torch.from_numpy(applied_transform).float() if applied_transform is not None else None,
-    # )
-    # out["ply_file_path"] = ply_filename
 
     with open(output_dir / "transforms.json", "w", encoding="utf-8") as f:
         json.dump(out, f, indent=4)
@@ -141,11 +99,10 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         "h": camera.height,
     }
 
-    # Parameters match https://github.com/colmap/colmap/blob/dev/src/base/camera_models.h
+    # Parameters match https://github.com/colmap/colmap/blob/main/src/colmap/sensor/models.h
     camera_params = camera.params
     if camera.model == "SIMPLE_PINHOLE":
-        # du = 0
-        # dv = 0
+        # f, cx, cy
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[0])
         out["cx"] = float(camera_params[1])
@@ -154,12 +111,9 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = 0.0
         out["p1"] = 0.0
         out["p2"] = 0.0
-        camera_model = CameraModel.OPENCV
+        camera_model = "OPENCV"
     elif camera.model == "PINHOLE":
-        # f, cx, cy, k
-
-        # du = 0
-        # dv = 0
+        # fx, fy, cx, cy
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[1])
         out["cx"] = float(camera_params[2])
@@ -168,14 +122,9 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = 0.0
         out["p1"] = 0.0
         out["p2"] = 0.0
-        camera_model = CameraModel.OPENCV
+        camera_model = "OPENCV"
     elif camera.model == "SIMPLE_RADIAL":
         # f, cx, cy, k
-
-        # r2 = u**2 + v**2;
-        # radial = k * r2
-        # du = u * radial
-        # dv = u * radial
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[0])
         out["cx"] = float(camera_params[1])
@@ -184,14 +133,9 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = 0.0
         out["p1"] = 0.0
         out["p2"] = 0.0
-        camera_model = CameraModel.OPENCV
+        camera_model = "OPENCV"
     elif camera.model == "RADIAL":
         # f, cx, cy, k1, k2
-
-        # r2 = u**2 + v**2;
-        # radial = k1 * r2 + k2 * r2 ** 2
-        # du = u * radial
-        # dv = v * radial
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[0])
         out["cx"] = float(camera_params[1])
@@ -200,15 +144,9 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = float(camera_params[4])
         out["p1"] = 0.0
         out["p2"] = 0.0
-        camera_model = CameraModel.OPENCV
+        camera_model = "OPENCV"
     elif camera.model == "OPENCV":
         # fx, fy, cx, cy, k1, k2, p1, p2
-
-        # uv = u * v;
-        # r2 = u**2 + v**2
-        # radial = k1 * r2 + k2 * r2 ** 2
-        # du = u * radial + 2 * p1 * u*v + p2 * (r2 + 2 * u**2)
-        # dv = v * radial + 2 * p2 * u*v + p1 * (r2 + 2 * v**2)
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[1])
         out["cx"] = float(camera_params[2])
@@ -217,23 +155,9 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = float(camera_params[5])
         out["p1"] = float(camera_params[6])
         out["p2"] = float(camera_params[7])
-        camera_model = CameraModel.OPENCV
+        camera_model = "OPENCV"
     elif camera.model == "OPENCV_FISHEYE":
         # fx, fy, cx, cy, k1, k2, k3, k4
-
-        # r = sqrt(u**2 + v**2)
-
-        # if r > eps:
-        #    theta = atan(r)
-        #    theta2 = theta ** 2
-        #    theta4 = theta2 ** 2
-        #    theta6 = theta4 * theta2
-        #    theta8 = theta4 ** 2
-        #    thetad = theta * (1 + k1 * theta2 + k2 * theta4 + k3 * theta6 + k4 * theta8)
-        #    du = u * thetad / r - u;
-        #    dv = v * thetad / r - v;
-        # else:
-        #    du = dv = 0
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[1])
         out["cx"] = float(camera_params[2])
@@ -242,20 +166,9 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = float(camera_params[5])
         out["k3"] = float(camera_params[6])
         out["k4"] = float(camera_params[7])
-        camera_model = CameraModel.OPENCV_FISHEYE
+        camera_model = "OPENCV_FISHEYE"
     elif camera.model == "FULL_OPENCV":
         # fx, fy, cx, cy, k1, k2, p1, p2, k3, k4, k5, k6
-
-        # u2 = u ** 2
-        # uv = u * v
-        # v2 = v ** 2
-        # r2 = u2 + v2
-        # r4 = r2 * r2
-        # r6 = r4 * r2
-        # radial = (1 + k1 * r2 + k2 * r4 + k3 * r6) /
-        #          (1 + k4 * r2 + k5 * r4 + k6 * r6)
-        # du = u * radial + 2 * p1 * uv + p2 * (r2 + 2 * u2) - u
-        # dv = v * radial + 2 * p2 * uv + p1 * (r2 + 2 * v2) - v
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[1])
         out["cx"] = float(camera_params[2])
@@ -279,16 +192,6 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         raise NotImplementedError(f"{camera.model} camera model is not supported yet!")
     elif camera.model == "SIMPLE_RADIAL_FISHEYE":
         # f, cx, cy, k
-
-        # r = sqrt(u ** 2 + v ** 2)
-        # if r > eps:
-        #     theta = atan(r)
-        #     theta2 = theta ** 2
-        #     thetad = theta * (1 + k * theta2)
-        #     du = u * thetad / r - u;
-        #     dv = v * thetad / r - v;
-        # else:
-        #     du = dv = 0
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[0])
         out["cx"] = float(camera_params[1])
@@ -297,21 +200,9 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = 0.0
         out["k3"] = 0.0
         out["k4"] = 0.0
-        camera_model = CameraModel.OPENCV_FISHEYE
+        camera_model = "OPENCV_FISHEYE"
     elif camera.model == "RADIAL_FISHEYE":
         # f, cx, cy, k1, k2
-
-        # r = sqrt(u ** 2 + v ** 2)
-        # if r > eps:
-        #     theta = atan(r)
-        #     theta2 = theta ** 2
-        #     theta4 = theta2 ** 2
-        #     thetad = theta * (1 + k * theta2)
-        #     thetad = theta * (1 + k1 * theta2 + k2 * theta4)
-        #     du = u * thetad / r - u;
-        #     dv = v * thetad / r - v;
-        # else:
-        #     du = dv = 0
         out["fl_x"] = float(camera_params[0])
         out["fl_y"] = float(camera_params[0])
         out["cx"] = float(camera_params[1])
@@ -320,14 +211,18 @@ def parse_colmap_camera_params(camera) -> Dict[str, Any]:
         out["k2"] = float(camera_params[4])
         out["k3"] = 0
         out["k4"] = 0
-        camera_model = CameraModel.OPENCV_FISHEYE
+        camera_model = "OPENCV_FISHEYE"
     else:
         # THIN_PRISM_FISHEYE not supported!
         raise NotImplementedError(f"{camera.model} camera model is not supported yet!")
 
-    out["camera_model"] = camera_model.value
+    out["camera_model"] = camera_model
     return out
 
 
 if __name__ == '__main__':
-    colmap_to_json(Path('../../results/glomap/ETH3D/courtyard/sparse/0'), Path('../../results/glomap/ETH3D/courtyard/'))
+    colmap_to_json(
+        recon_dir=Path('../../results/glomap/ETH3D/courtyard/sparse/0'),
+        output_dir=Path('../../results/glomap/ETH3D/courtyard/'),
+        image_dir='../../data/ETH3D/courtyard/images/',
+    )
