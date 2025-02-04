6ed0467332476fd2fa3405494e2cb58c5a9dce47
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-02-04 14:22:26 +0100

    Fix paths

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -23,12 +23,12 @@ cd "$(dirname "$0")/.." || exit
 # VGGSfm
 for scene in "${SCENES[@]}"; do
   # Check if the estimated model exists
-  if [ ! -d "results/vggsfm/ETH3D/${scene}/sparse" ]; then
+  if [ ! -d "results/vggsfm/ETH3D/${scene}/colmap/sparse" ]; then
     echo "VGGSfm model not found for ${scene}. Skipping..."
     continue
   fi
   echo "VGGSfm: Processing ${scene}"
-  python main.py --est-model-path results/vggsfm/ETH3D/${scene}/sparse --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
+  python main.py --est-model-path results/vggsfm/ETH3D/${scene}/colmap/sparse --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
 done
 
 # Flowmap
@@ -56,10 +56,10 @@ done
 # Glomap
 for scene in "${SCENES[@]}"; do
   # Check if the estimated model exists
-  if [ ! -d "results/glomap/ETH3D/${scene}/sparse/0" ]; then
+  if [ ! -d "results/glomap/ETH3D/${scene}/colmap/sparse/0" ]; then
     echo "Glomap model not found for ${scene}. Skipping..."
     continue
   fi
   echo "Glomap: Processing ${scene}"
-  python main.py --est-model-path results/glomap/ETH3D/${scene}/sparse/0 --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
+  python main.py --est-model-path results/glomap/ETH3D/${scene}/colmap/sparse/0 --gt-model-path datasets/ETH3D/${scene}/dslr_calibration_jpg
 done
