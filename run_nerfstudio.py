0dc5427cced5952b691ccf6617a1fbe9bafcbed6
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-02-07 10:38:33 +0100

    fix unsupported num-workers for splatfacto

--- run_nerfstudio.py
+++ run_nerfstudio.py
@@ -58,7 +58,7 @@ def run_nerfstudio(dataset_path, results_path, method='nerfacto', viz=False):
     _logger.info(f"Training the model using : {method}")
     train_cmd = (f"{CUDA_VISIBLE_DEVICES} ns-train {method} "
            f"--machine.num-devices {num_gpus} --pipeline.datamanager.images-on-gpu True "
-           f"--pipeline.datamanager.dataloader-num-workers 8"
+           f"{'--pipeline.datamanager.dataloader-num-workers 8' if method == 'nerfacto' else ''} "
            f"{'--viewer.make-share-url True' if viz else ''} "
            f"--images-path {dataset_path}/images --colmap-path {results_path}/colmap/sparse/0 "
            f" --output-dir {results_path}/nerfstudio colmap")
