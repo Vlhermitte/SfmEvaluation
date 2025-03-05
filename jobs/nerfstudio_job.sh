0c3adc3fbf3ee88ac73d6983c51313c616ba288d
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-03-05 17:47:59 +0100

    Update job script to improve directory navigation based on SLURM job context

--- jobs/nerfstudio_job.sh
+++ jobs/nerfstudio_job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=nerfstudio_job      # Job name
 #SBATCH --output=nerfstudio_out.log    # Standard output log
 #SBATCH --error=nerfstudio_err.log     # Standard error log
-#SBATCH --time=12:00:00                # Time lim
+#SBATCH --time=16:00:00                # Time limit in the format HH:MM:SS
 #SBATCH --partition=1day               # Use the '1day' partition
 #SBATCH --gres=gpu:a16:1               # Request 1 GPU (a16)
 #SBATCH --mem=16G                      # Memory allocation
