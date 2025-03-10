12a2d9e49ee48dff8b0742b672d533d1b98aff00
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-03-10 19:04:16 +0100

    Refactor run_vggsfm.sh to streamline Conda environment activation and improve error handling

--- scripts/run_vggsfm.sh
+++ scripts/run_vggsfm.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=vggsfm_job
-#SBATCH --output=vggsfm_job.out
-#SBATCH --error=vggsfm_job.err
-#SBATCH --time=04:00:00
-#SBATCH --partition=fast
-#SBATCH --gres=gpu:a16:1
-#SBATCH --mem=24G
-#SBATCH --cpus-per-task=8
-
-if [ ! -z "$SLURM_JOB_ID" ]; then
-    echo "Running on a Slurm-managed system. Loading required modules..."
-    module load Anaconda3
-fi
-
 scene=$1
 out=$2
 
@@ -22,6 +8,31 @@ if [ -z "$scene" ] || [ -z "$out" ]; then
     echo "Usage: ./run_vggsfm.sh <scene_dir> <output_dir>"
     exit 1
 fi
+
+if [ ! -z "$SLURM_JOB_ID" ]; then
+    echo "Running on a Slurm-managed system. Loading required modules..."
+    module load Anaconda3
+fi
+
+source "$(conda info --base)/etc/profile.d/conda.sh"
+
+conda_env="vggsfm_tmp"
+conda activate $conda_env || { echo "ERROR: Failed to activate Conda environment: $conda_env"; exit 1; }
+
+# Check if the correct Conda environment is active
+if [[ "$CONDA_PREFIX" != *"$conda_env"* ]]; then
+    echo "ERROR: Conda environment $conda_env is not activated correctly!"
+    echo "Current Conda environment: $CONDA_PREFIX"
+    exit 1
+fi
+
+echo "Conda environment activated: $CONDA_PREFIX"
+
+# Check Python path
+python_path=$(which python)
+echo "Using Python: $python_path"
+
+python -c "import lightglue; print('LightGlue is working')"
 
 out_dir=$out
 echo "Output directory: $out_dir"
@@ -58,12 +69,6 @@ else
     echo "Scene directory does not have a subdirectory named 'images'"
     exit 1
 fi
-
-
-conda_env="vggsfm_tmp"
-echo "Activating conda environment: $conda_env"
-source "$(conda info --base)/etc/profile.d/conda.sh" || { echo "Failed to source conda.sh"; exit 1; }
-conda activate $conda_env || { echo "Failed to activate conda environment: $conda_env"; exit 1; }
 
 # run the VGG-SfM pipeline
 start_time=$(date +%s)
