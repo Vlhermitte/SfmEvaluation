3a423d7a7ccaa12f1163abddc83ca45835308708
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-03-30 10:35:40 +0200

    Refactor boolean argument parsing to use strtobool for improved clarity

--- src/run_nerfstudio.py
+++ src/run_nerfstudio.py
@@ -182,15 +182,8 @@ def sanity_check_colmap(path: Path) -> None:
 
 
 if __name__ == '__main__':
-    def str2bool(v):
-        if isinstance(v, bool):
-            return v
-        if v.lower() in ("yes", "true", "t", "1"):
-            return True
-        elif v.lower() in ("no", "false", "f", "0"):
-            return False
-        else:
-            raise argparse.ArgumentTypeError("Boolean value expected.")
+    def _strtobool(x):
+        return bool(strtobool(x))
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -217,7 +210,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         "--viz",
-        type=str2bool,
+        type=_strtobool,
         required=False,
         default=True,
         help="Whether to visualize the results"
