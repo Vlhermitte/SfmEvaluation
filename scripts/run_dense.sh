8707178eaf21606ae5cda903d0b9ab97add7ca28
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-03-31 13:45:05 +0200

    Add dense reconstruction script

--- scripts/run_dense.sh
+++ scripts/run_dense.sh
@@ -9,6 +9,10 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32G
 
+log() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+}
+
 # Check if the script is running on a Slurm device
 if [ ! -z "$SLURM_JOB_ID" ]; then
     echo "Running on a Slurm-managed system. Loading required modules..."
@@ -21,20 +25,21 @@ if ! command -v colmap &> /dev/null; then
     exit 1
 fi
 
-scene=$1
-out=$2
+image_dir=$1
+sparse_dir=$2
+out=$3
 
-# Check if the scene and output directory are provided
-if [ -z "${scene}" ]; then
-    echo "Usage: ./run_dense.sh <scene_dir> <output_dir>"
+# Check if the sparse_dir and output directory are provided
+if [ -z "${sparse_dir}" ]; then
+    echo "Usage: ./run_dense.sh <image_path> <sparse_dir> <output_dir>"
     exit 1
 fi
 
 out_dir=$out
 
-# Check if the scene directory exists
-if [ ! -d "${scene}" ]; then
-    echo "Scene directory does not exist"
+# Check if the sparse_dir directory exists
+if [ ! -d "${sparse_dir}" ]; then
+    log "Scene directory does not exist"
     exit 1
 fi
 
@@ -46,7 +51,7 @@ fi
 if [ -z "$SLURM_JOB_ID" ]; then
   # Check if the output directory is not empty and prompt for overwrite
   if [ -d "${out_dir}" ] && [ "$(ls -A "${out_dir}")" ]; then
-      echo "Output directory '$out_dir' is not empty. Do you want to overwrite? (y/n)"
+      log "Output directory '$out_dir' is not empty. Do you want to overwrite? (y/n)"
       read -r answer
       case "$answer" in
           y|Y)
@@ -67,20 +72,24 @@ fi
 
 mkdir -p "${out_dir}"
 
-echo "Running dense reconstruction on scene: ${scene}"
+gpu_name=$(nvidia-smi --query-gpu=name --format=csv,noheader | head -n 1)
+log "Running dense reconstruction on sparse_dir: ${sparse_dir} with GPU: ${gpu_name}"
 start_time=$(date +%s)
 
+log "Image undistortion..."
 colmap image_undistorter \
-    --image_path "${scene}" \
-    --input_path "${scene}" \
+    --image_path "${image_dir}" \
+    --input_path "${sparse_dir}/0" \
     --output_path "${out_dir}" \
     --output_type COLMAP
 
+log "PatchMatch Stereo..."
 colmap patch_match_stereo \
    --workspace_path "${out_dir}" \
    --workspace_format COLMAP \
    --PatchMatchStereo.geom_consistency true
 
+log "Stereo Fusion..."
 colmap stereo_fusion \
    --workspace_path "${out_dir}" \
    --workspace_format COLMAP \
@@ -90,4 +99,4 @@ colmap stereo_fusion \
 end_time=$(date +%s)
 elapsed_time=$(( end_time - start_time ))
 
-echo "Elapsed time: $elapsed_time seconds" >> "${out_dir}"/dense_time.txt
+echo "Elapsed time: $elapsed_time seconds on ${gpu_name}" >> "${out_dir}"/dense_time.txt
