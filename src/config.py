e137f065c9b04e457a02755e09cc13b19468062a
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-04-04 19:53:39 +0200

    Remove unused path for ETH3D Multi-view evaluation tool and add COLMAP dense format

--- src/config.py
+++ src/config.py
@@ -1,8 +1,5 @@
 from pathlib import Path
 
-# Path to the ETH3D Multi-view evaluation tool
-MULTIVIEW_EVALUATION_TOOL_PATH = Path("../multi-view-evaluation/build/ETH3DMultiViewEvaluation")
-
 # Data Path
 ETH3D_DATA_PATH = Path("../data/datasets/ETH3D")
 MIP_NERF_360_DATA_PATH = Path("../data/datasets/MipNerf360")
@@ -53,3 +50,4 @@ FLOWMAP_RESULTS_PATH = RESULTS_PATH / "flowmap"
 VGGSFM_RESULTS_PATH = RESULTS_PATH / "vggsfm"
 ACEZERO_RESULTS_PATH = RESULTS_PATH / "acezero"
 COLMAP_FORMAT = "colmap/sparse/0"
+COLMAP_DENSE_FORMAT = "colmap/dense/"
