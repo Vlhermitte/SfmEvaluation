64af00edafe809d4956af06f3619df2993a699f1
Vlhermitte <valentin.lhermitte2604@gmail.com>
2025-05-17 09:09:35 +0200

    refactor config.py to use absolute paths for dataset directories and add new LaMAR dataset paths

--- src/config.py
+++ src/config.py
@@ -1,9 +1,17 @@
+import os
 from pathlib import Path
 
+import sys
+
+path = Path(__file__).parent.parent
+
 # Data Path
-ETH3D_DATA_PATH = Path("../data/datasets/ETH3D")
-MIP_NERF_360_DATA_PATH = Path("../data/datasets/MipNerf360")
-TANKS_AND_TEMPLES_DATA_PATH = Path("../data/datasets/TanksAndTemples")
+ETH3D_DATA_PATH = Path(path / "data/datasets/ETH3D")
+MIP_NERF_360_DATA_PATH = Path(path / "data/datasets/MipNerf360")
+TANKS_AND_TEMPLES_DATA_PATH = Path(path / "data/datasets/TanksAndTemples")
+LAMAR_HGE_DATA_PATH = Path(path / "data/datasets/LaMAR/HGE/sessions/map/raw_data/")
+LAMAR_CAB_DATA_PATH = Path(path / "data/datasets/LaMAR/CAB/sessions/map/raw_data/")
+LAMAR_LIN_DATA_PATH = Path(path / "data/datasets/LaMAR/LIN/sessions/map/raw_data/")
 
 ETH3D_SCENES = (
         "courtyard",
@@ -41,10 +49,15 @@ TANKS_AND_TEMPLES_SCENES = (
     "Truck"
 )
 
-METHODS = ['Glomap', 'VGGSfm', 'FlowMap', 'AceZero']
+LAMAR_HGE_SCENES = sorted([p.name for p in LAMAR_HGE_DATA_PATH.glob("ios*") if p.is_dir()])
+LAMAR_CAB_SCENES = sorted([p.name for p in LAMAR_CAB_DATA_PATH.glob("ios*") if p.is_dir()])
+LAMAR_LIN_SCENES = sorted([p.name for p in LAMAR_LIN_DATA_PATH.glob("ios*") if p.is_dir()])
+
+METHODS = ['COLMAP', 'GLOMAP', 'VGGSfM', 'FlowMap', 'AceZero']
 
 # Results Path
-RESULTS_PATH = Path("../data/results/")
+RESULTS_PATH = Path(path / "data/results/").absolute()
+COLMAP_RESULTS_PATH = RESULTS_PATH / "colmap"
 GLOMAP_RESULTS_PATH = RESULTS_PATH / "glomap"
 FLOWMAP_RESULTS_PATH = RESULTS_PATH / "flowmap"
 VGGSFM_RESULTS_PATH = RESULTS_PATH / "vggsfm"
